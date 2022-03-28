7c0d76d69d21717108a4f51a9367b968fdd5d01c
priti123-meshram <80310091+priti123-meshram@users.noreply.github.com>
2022-03-28 10:37:42 +0530

    Add files via upload

---
+++ update.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+fs.appendFile('mynewfile1.txt', ' This is my text.', function (err) {
+}); 
+
+
+// fs.writeFile()
+// var fs = require('fs');
+
+// fs.writeFile('mynewfile3.txt', 'This is my text', function () {
+// }); 
+
+
